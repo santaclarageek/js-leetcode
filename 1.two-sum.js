cc3e9bee35421f392c32344620331f19981879a1
YueYao <yueyao@ucdavis.edu>
2018-08-18 19:17:57 -0700

    Create 1.two-sum.js

---
+++ 1.two-sum.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=1 lang=javascript
+ *
+ * [1] Two Sum
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var result = [];
+    var temptarget = target;
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i+1; j < nums.length; j++){
+            if(nums[i] + nums[j] === target){
+                result[0] = i;
+                result[1] = j;
+            }
+        }
+    }
+    return result;
+};
+
