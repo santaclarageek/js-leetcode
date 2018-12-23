d593f4a4c5962fe0dc3cf5ae9e115803343d88b0
Jonathan Zhenqian Guo <gzqsjtu@163.com>
2018-12-22 22:59:35 -0800

    fix bugs

--- Consecutive.js
+++ Consecutive.js
@@ -1,4 +1,24 @@
 	// longest consecutive
+
+	process.stdin.resume();
+process.stdin.setEncoding('utf8');
+const fs = require('fs');
+var stdin = '';
+
+
+process.stdin.on('data', function (chunk) {
+  stdin += chunk;
+  
+}).on('end', function() {
+  var lines = stdin.trim().split('\n');
+  
+  for(var i=0; i<lines.length; i++) {
+    process.stdout.write(longestConsecutive(parseInt(lines[i])).toString());
+  }
+});
+
+
+
 	var longestConsecutive = function(nums) {
 
 
